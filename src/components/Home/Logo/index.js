5ef4c1327807fdc411561af1613d24fae8fe7047
Alexander Coy <alexcoy06@gmail.com>
2023-12-20 11:10:23 -0600

    Added Logo Fix

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import LogoS from '../../../assets/images/logo-s.png'
+import { useRef } from 'react'
+
+const Logo = () => {
+    
+    return (
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoS} alt='S' />
+        </div>
+    )
+}
+
+export default Logo
