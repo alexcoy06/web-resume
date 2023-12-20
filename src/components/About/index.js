b9fcb66ea7eda1dad7e2c9729f4c51e69eb16de5
Alexander Coy <alexcoy06@gmail.com>
2023-12-20 15:04:41 -0600

    About me page added

---
+++ src/components/About/index.js
@@ -0,0 +1,66 @@
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react'
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 4000);
+      
+        return () => clearTimeout(timeoutId);
+      }, []);
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                    idx={15}
+                />
+                </h1>
+                <p>
+                    text
+                </p>
+                <p>
+                    text
+                </p>
+                <p>
+                    text
+                </p>
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon  icon={faAngular} color='#dd0031'  />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon  icon={faHtml5} color='#f06529'  />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon  icon={faCss3} color='#28a4d9'  />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon  icon={faReact} color='#5ed4f4'  />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon  icon={faJsSquare} color='#efd81d'  />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon  icon={faGitAlt} color='#ec4d28'  />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
