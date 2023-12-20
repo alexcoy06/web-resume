4a9ff08733df0ae40597954f3f0907332e651325
Alexander Coy <alexcoy06@gmail.com>
2023-12-20 16:14:44 -0600

    Added Loaders to Home and About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
 
@@ -17,49 +18,52 @@ const About = () => {
       }, []);
 
     return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                <AnimatedLetters 
-                    letterClass={letterClass}
-                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                    idx={15}
-                />
-                </h1>
-                <p>
-                    text
-                </p>
-                <p>
-                    text
-                </p>
-                <p>
-                    text
-                </p>
-            </div>
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                    />
+                    </h1>
+                    <p>
+                        text
+                    </p>
+                    <p>
+                        text
+                    </p>
+                    <p>
+                        text
+                    </p>
+                </div>
 
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon  icon={faAngular} color='#dd0031'  />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon  icon={faHtml5} color='#f06529'  />
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon  icon={faCss3} color='#28a4d9'  />
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon  icon={faReact} color='#5ed4f4'  />
-                    </div>
-                    <div className='face5'>
-                        <FontAwesomeIcon  icon={faJsSquare} color='#efd81d'  />
-                    </div>
-                    <div className='face6'>
-                        <FontAwesomeIcon  icon={faGitAlt} color='#ec4d28'  />
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon  icon={faAngular} color='#dd0031'  />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon  icon={faHtml5} color='#f06529'  />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon  icon={faCss3} color='#28a4d9'  />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon  icon={faReact} color='#5ed4f4'  />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon  icon={faJsSquare} color='#efd81d'  />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon  icon={faGitAlt} color='#ec4d28'  />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import React, { useState, useEffect } from 'react';
 import Logo from './Logo';
 import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -19,28 +20,31 @@ const Home = () => {
       }, []);
     
     return (
-        <div className="container home-page">
-            <div className='text-zone'>
-                <h1>
-                    <span className={letterClass}>H</span> 
-                    <span className={`${letterClass} _12`}>i,</span> 
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <img src={LogoTitle} alt='developer' /> 
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15} />
-                    <br />
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={jobArray}
-                    idx={23} />
-                </h1>
-                <h2>Frontend Developer / Javascript / HTML / CSS </h2>
-                <Link to='/contact' className='flat-button' >CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span> 
+                        <span className={`${letterClass} _12`}>i,</span> 
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img src={LogoTitle} alt='developer' /> 
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={23} />
+                    </h1>
+                    <h2>Frontend Developer / Javascript / HTML / CSS </h2>
+                    <Link to='/contact' className='flat-button' >CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
